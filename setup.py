5ced183d3ee0958bf3abc0ce4a08943ab77ffcb0
Aman choudhary <129528281+amankumarchy5423@users.noreply.github.com>
2026-06-02 09:42:39 +0000

    logger and utils function written

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+from typing import List
+
+
+setup(
+    name="Kidney-disease",
+    version="1.0.0",
+    author="Aman Kumar Choudhary",
+    author_email="amankumarchy5423@gmail.com",
+    packages= find_packages(),
+    
+
+)
